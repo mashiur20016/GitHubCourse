35deeec70613a2c795db7684928c895204bec341
mashiur20016 <mashiur20016@gmail.com>
2022-01-04 14:02:20 -0500

    files added/deleted

---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+public class SearchPage {
+	
+	public void search() {
+		
+		System.out.println("Search");
+		String un = "admin";
+		String pwd = " admin123";
+		System.out.println("Logged in ......");
+	}
+
+}
